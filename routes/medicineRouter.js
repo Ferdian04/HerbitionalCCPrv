796058d0507066a218f5b59f3197eac5fc02e859
Yahyap <90821669+Yahyap@users.noreply.github.com>
2023-05-26 21:25:40 +0700

    Delete routes directory

--- routes/medicineRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const passport = require("passport");
-const router = express.Router();
-const medicineController = require("../controllers/medicineController");
-
-//router.get("/json", passport.authenticate("jwt", { session: false }), medicineController.getMed);
-router.post("/med"), medicineController.getMed;
-
-module.exports = router;
