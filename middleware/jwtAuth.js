d6cb60866d96b3d6ec6cddc0b38cbb863ae422e0
Ferdian Hafiz <89018562+Ferdian04@users.noreply.github.com>
2023-05-29 22:54:22 +0700

    Update jwtAuth.js

--- middleware/jwtAuth.js
+++ middleware/jwtAuth.js
@@ -11,7 +11,7 @@ const jwtAuth = () => {
       return res.status(401).json({ error: "Unauthorized" });
     }
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    jwt.verify(token, "SECRET", (err, decoded) => {
       if (err) {
         return res.status(401).json({ error: "Invalid token" });
       }
